892c43c127c1b42ad5e2018e031ad9819127d50e
NhanItJAVA1 <nhantnpt2609@gmail.com>
2024-03-31 21:44:42 +0700

    editting signup

--- JS/appSignUp.js
+++ JS/appSignUp.js
@@ -1,12 +1,25 @@
-const main = document.querySelector('.wrapper')
+const wrapper = document.querySelector('.wrapper')
 const loginLink = document.querySelector('.login-link')
 const registerLink = document.querySelector('.register-link')
 
 
+const formBoxRegister = document.querySelector('.form-box.register')
+const formBoxLogin = document.querySelector('.form-box.login')
+
+
 registerLink.addEventListener('click',() => {
-    main.classList.add('active');
+    wrapper.classList.add('active');
+    formBoxLogin.style.display = "none";
+    formBoxRegister.style.display = "flex";
+    wrapper.style.background ="url(../IMAGE/BackGroundRegister.jpg) no-repeat";
 })
 
+
+
 loginLink.addEventListener('click',() => {
-    main.classList.remove('active');
+    wrapper.classList.remove('active');
+    formBoxRegister.style.display = "none";
+    formBoxLogin.style.display = "flex";
+    wrapper.style.background = "url(../IMAGE/BackGroud_SignUp.jpg) no-repeat";
+
 })
