a82eb0a41ad8e0f460b4f1852b70377461234e53
Hung ne <hungok2909@gmail.com>
2024-04-13 22:40:10 +0700

    handle event for select input

--- JS/parseMovies.js
+++ JS/parseMovies.js
@@ -1,7 +1,48 @@
 $(document).ready(function(){
   var data;
+    function displayFilm(type, year, category){
+      var count = 0;
+      for(let i of data){
+        if(type == "all" || (i.type == type && i.year == year && i.category == category)){
+          count++;
+          $("#phimnb").append($("<div class=\"movie-ticket\">")
+                              .append($("<div class=\"container-fluid\">")
+                              .append($("<div class= \"ui-item\">")
+                              .append($("<div class=\"ui-head\">")
+                              .append($("<img src=\"" + i.imgPath + "\" alt=\"\" class=\"img-responsive bg-img\">"),
+                                      $("<div class=\"ui-trans clearfix d-flex align-items-center\">")
+                              .append($("<div>")
+                              .append($("<img src=\"" + i.imgPath + "\" alt=\"\" class=\"img-responsive\">"),
+                                      $("<div class=\"ui-details clearfix\">")
+                              .append($("<h2>").append($("<a href=\"#\">").text(i.name)),
+                                      $("<a href=\"#\" class=\"label\">").text(i.year),
+                                      $("<a href=\"#\" class=\"label\">").text(i.duration + " phút"),
+                                      $("<p>").text(i.description),
+                                      $("<h4>").append($("<span>").text("Đạo diễn:")),
+                                      $("<h5>").append($("<a href=\"#\">").text(i.director)),
+                                      $("<h4>").append($("<span>").text("Diễn viên:")),
+                                      $("<h5>").append($("<a href=\"#\">").text(i.actors)),
+                                      $("<h4>").append($("<span>").text("Khởi chiếu:")),
+                                      $("<h5>").append($("<a href=\"#\">").text(i.released))
+                                      ))))
+                              ,
+                              $("<div class=\"ui-review text-center\">")
+                              .append($("<a href=\"#\" class=\"ui-btn bg-red\">")
+                              .append($("<i class=\"fa fa-ticket\">")
+                              .text("Đặt vé")), 
+                                     $("<a href=\"#\" class=\"ui-btn bg-green\">")
+                              .append($("<i class=\"fa fa-dot-circle-o\">")
+                              .text("Buy DVDs"))))))
+          );
+        }
+      }
+      if(count == 0)
+        $("#phimnb").append($("<div class=\"container-fluid d-flex justify-content-center align-items-center\" style=\"height: 500px\">").append($("<h1 class=\"text-light\">").text("Không tìm thấy phim:(")));
+    }
     $.getJSON('../DATA/movies.json', function(moviesData) {
       data = moviesData;
+      $("#year-select").prop('disabled', true);
+      $("#cate-select").prop('disabled', true);
         for(let i of moviesData){
             $("#phimnb").append($("<div class=\"movie-ticket\">")
                               .append($("<div class=\"container-fluid\">")
@@ -39,39 +80,31 @@ $(document).ready(function(){
     });
     $("#film-select").change(function(){
       $("#phimnb").empty();
-      console.log($(this).val())
-      for(let i of data){
-        if(i.type == $(this).val()){
-          $("#phimnb").append($("<div class=\"movie-ticket\">")
-          .append($("<div class=\"container-fluid\">")
-          .append($("<div class= \"ui-item\">")
-          .append($("<div class=\"ui-head\">")
-          .append($("<img src=\"" + i.imgPath + "\" alt=\"\" class=\"img-responsive bg-img\">"),
-                  $("<div class=\"ui-trans clearfix d-flex align-items-center\">")
-          .append($("<div>")
-          .append($("<img src=\"" + i.imgPath + "\" alt=\"\" class=\"img-responsive\">"),
-                  $("<div class=\"ui-details clearfix\">")
-          .append($("<h2>").append($("<a href=\"#\">").text(i.name)),
-                  $("<a href=\"#\" class=\"label\">").text(i.year),
-                  $("<a href=\"#\" class=\"label\">").text(i.duration + " phút"),
-                  $("<p>").text(i.description),
-                  $("<h4>").append($("<span>").text("Đạo diễn:")),
-                  $("<h5>").append($("<a href=\"#\">").text(i.director)),
-                  $("<h4>").append($("<span>").text("Diễn viên:")),
-                  $("<h5>").append($("<a href=\"#\">").text(i.actors)),
-                  $("<h4>").append($("<span>").text("Khởi chiếu:")),
-                  $("<h5>").append($("<a href=\"#\">").text(i.released))
-                  ))))
-          ,
-          $("<div class=\"ui-review text-center\">")
-          .append($("<a href=\"#\" class=\"ui-btn bg-red\">")
-          .append($("<i class=\"fa fa-ticket\">")
-          .text("Đặt vé")), 
-                 $("<a href=\"#\" class=\"ui-btn bg-green\">")
-          .append($("<i class=\"fa fa-dot-circle-o\">")
-          .text("Buy DVDs"))))))
-          );
-        }
+      var type = $(this).val();
+      if(type == "all"){
+        $("#year-select").prop('disabled', true);
+        $("#cate-select").prop('disabled', true);
       }
+      else{
+        $("#year-select").prop('disabled', false);
+        $("#cate-select").prop('disabled', false);
+      }
+      var year = $("#year-select").val();
+      var category = $("#cate-select").val();
+      displayFilm(type, year, category);
+    })
+    $("#year-select").change(function(){
+      $("#phimnb").empty();
+      var type = $("#film-select").val();
+      var year = $(this).val();
+      var category = $("#cate-select").val();
+      displayFilm(type, year, category);
+    })
+    $("#cate-select").change(function(){
+      $("#phimnb").empty();
+      var type = $("#film-select").val();
+      var year = $("#year-select").val();
+      var category = $(this).val();
+      displayFilm(type, year, category);
     })
 })
