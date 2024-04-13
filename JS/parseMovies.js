db499149e0f0aa3a4d5a43cc2db22527526cdda8
Hung ne <hungok2909@gmail.com>
2024-04-13 11:43:21 +0700

    add search bar and option select

--- JS/parseMovies.js
+++ JS/parseMovies.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+  var data;
     $.getJSON('../DATA/movies.json', function(moviesData) {
+      data = moviesData;
         for(let i of moviesData){
-            $("#film-contain").append($("<div class=\"movie-ticket\">")
+            $("#phimnb").append($("<div class=\"movie-ticket\">")
                               .append($("<div class=\"container-fluid\">")
                               .append($("<div class= \"ui-item\">")
                               .append($("<div class=\"ui-head\">")
@@ -35,4 +37,41 @@ $(document).ready(function(){
     .fail(function(jqXHR, textStatus, errorThrown) {
         console.error('Error fetching movies:', textStatus, errorThrown);
     });
+    $("#film-select").change(function(){
+      $("#phimnb").empty();
+      console.log($(this).val())
+      for(let i of data){
+        if(i.type == $(this).val()){
+          $("#phimnb").append($("<div class=\"movie-ticket\">")
+          .append($("<div class=\"container-fluid\">")
+          .append($("<div class= \"ui-item\">")
+          .append($("<div class=\"ui-head\">")
+          .append($("<img src=\"" + i.imgPath + "\" alt=\"\" class=\"img-responsive bg-img\">"),
+                  $("<div class=\"ui-trans clearfix d-flex align-items-center\">")
+          .append($("<div>")
+          .append($("<img src=\"" + i.imgPath + "\" alt=\"\" class=\"img-responsive\">"),
+                  $("<div class=\"ui-details clearfix\">")
+          .append($("<h2>").append($("<a href=\"#\">").text(i.name)),
+                  $("<a href=\"#\" class=\"label\">").text(i.year),
+                  $("<a href=\"#\" class=\"label\">").text(i.duration + " phút"),
+                  $("<p>").text(i.description),
+                  $("<h4>").append($("<span>").text("Đạo diễn:")),
+                  $("<h5>").append($("<a href=\"#\">").text(i.director)),
+                  $("<h4>").append($("<span>").text("Diễn viên:")),
+                  $("<h5>").append($("<a href=\"#\">").text(i.actors)),
+                  $("<h4>").append($("<span>").text("Khởi chiếu:")),
+                  $("<h5>").append($("<a href=\"#\">").text(i.released))
+                  ))))
+          ,
+          $("<div class=\"ui-review text-center\">")
+          .append($("<a href=\"#\" class=\"ui-btn bg-red\">")
+          .append($("<i class=\"fa fa-ticket\">")
+          .text("Đặt vé")), 
+                 $("<a href=\"#\" class=\"ui-btn bg-green\">")
+          .append($("<i class=\"fa fa-dot-circle-o\">")
+          .text("Buy DVDs"))))))
+          );
+        }
+      }
+    })
 })
